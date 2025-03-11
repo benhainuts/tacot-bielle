dd9f4cd98c5656878b91c7c53f198eca6ebe19b4
benoit hainaut <benoit.hainaut@gmail.com>
2025-03-11 14:30:21 +0100

    cars ok

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,22 +1,48 @@
 class CarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :new, :create, :show, :edit, :update]
+  before_action :set_car, only: [:edit, :update, :show]
 
   def index
+    @cars = Car.all
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(car_params)
+    @car.user = current_user
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    # @plan_item = @car.plan_items.new
   end
 
   def edit
   end
 
   def update
+    if @car.update(car_params)
+      redirect_to car_path(@car)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+private
+
+def car_params
+  params.require(:car).permit(:make, :model, :mileage, :date_of_first_purchase, :estimated_mileage_per_year)
+end
+
+def set_car
+  @car = Car.find(params[:id])
+end
 
 end
--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -1,13 +1,24 @@
 class PlanItemsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_car
+  before_action :set_plan_item, only: [:show, :edit, :update]
 
   def index
+    @plan_items = PlanItem.all
   end
 
   def new
+    @plan_item = @car.plan_items.new
   end
 
   def create
+    @plan_item = @car.plan_items.new(plan_item_params)
+    @plan_item.user = current_user
+    if @plan_item.save
+      redirect_to car_path(@car), notice: "Plan d'entretien mis à jour"
+    else
+      render car_path(@car), status: :unprocessable_entity
+    end
   end
 
   def show
@@ -17,8 +28,28 @@ class PlanItemsController < ApplicationController
   end
 
   def update
+    if @plan_item.update(plan_item_params)
+      redirect_to car_path(@car), notice: "Plan d'entretien mis à jour"
+    else
+      render car_path(@car), status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
+
+private
+
+def set_car
+  @car = Car.find(params[:car_id])
+end
+
+def set_plan_item
+  @plan_item = PlanItem.find(params[:id])
+end
+
+def plan_item_params
+  params.require(:plan_item).permit(:name, :to_do_every_x_km, :to_do_every_x_years)
+end
+
 end
